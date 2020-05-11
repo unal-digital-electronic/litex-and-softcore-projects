8b74d6eca88457c1f1d2a99f91afdf52639646f4
Johnny Germán Cubides Castro <jgcubidesc@gmail.com>
2020-05-10 19:56:55 -0500

    init blink for colorlight 5A-75E

---
+++ openocd-tool/host/omega2/colorlight-5A-75E-blink/README.md
@@ -0,0 +1,4 @@
+## Referencias
+
+[Configuración openocd para ecp5-x](https://github.com/f32c/f32c/tree/master/rtl/proj/lattice/programmer/openocd/ulx3s)
+
---
+++ openocd-tool/host/omega2/colorlight-5A-75E-blink/program.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# BOOT GPIO0
+# TRST GPIO1
+# BOOT0 = 0 Main flash
+# BOOT0 = 1 Embedded SRAM
+omega2-ctrl gpiomux set pwm0 gpio
+omega2-ctrl gpiomux set pwm1 gpio
+# Boot0 = 1
+echo "0" >  "/sys/class/gpio/unexport"
+echo "0" >  "/sys/class/gpio/export"
+echo "out" > "/sys/class/gpio/gpio0/direction"
+echo "1" >   "/sys/class/gpio/gpio0/value"
+# Reset
+echo "19" >  "/sys/class/gpio/unexport"
+echo "19" >  "/sys/class/gpio/export"
+echo "out" > "/sys/class/gpio/gpio19/direction"
+echo "1" >   "/sys/class/gpio/gpio19/value"
+sleep 1 
+echo "0" >  "/sys/class/gpio/gpio19/value"
+sleep 1  
+echo "1" >   "/sys/class/gpio/gpio19/value"
+openocd -f ecp5-25f.cfg 
+# Boot0 = 1
+echo "0" >  "/sys/class/gpio/unexport"
+echo "0" >  "/sys/class/gpio/export"
+echo "out" > "/sys/class/gpio/gpio0/direction"
+echo "0" >   "/sys/class/gpio/gpio0/value"
+# Reset
+echo "19" >  "/sys/class/gpio/unexport"
+echo "19" >  "/sys/class/gpio/export"
+echo "out" > "/sys/class/gpio/gpio19/direction"
+echo "1" >   "/sys/class/gpio/gpio19/value"
+sleep 1 
+echo "0" >  "/sys/class/gpio/gpio19/value"
+sleep 1  
+echo "1" >   "/sys/class/gpio/gpio19/value"
+
+
